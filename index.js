5547d1476e60abbf2725e3152c14bae7a1b11353
Eugene Sharygin <eush77@gmail.com>
2015-10-31 11:57:38 +0300

    expand tildes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var rc = require('rc');
+var rc = require('rc'),
+    untildify = require('untildify');
 
 var path = require('path');
 
 
 module.exports = function () {
-  return rc('npm', null, []).prefix ||
+  return untildify(rc('npm', null, []).prefix) ||
     (process.platform == 'win32'
      ? path.dirname(process.execPath)
      : path.resolve(process.execPath, '../..'));
--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@
 
 var npmPrefix = require('..');
 
-var test = require('tape');
+var test = require('tape'),
+    homedir = require('os-homedir');
 
 var path = require('path');
 
@@ -10,5 +11,12 @@ var path = require('path');
 test('absolute prefix', function (t) {
   process.chdir(__dirname + '/fixtures/absolute');
   t.equal(npmPrefix(), '/root');
+  t.end();
+});
+
+
+test('home-relative prefix', function (t) {
+  process.chdir(__dirname + '/fixtures/home-relative');
+  t.equal(npmPrefix(), path.join(homedir(), 'local'));
   t.end();
 });
