9af215d485b9a66a5bd490919b9b1beaad00b9d7
Eugene Sharygin <eush77@gmail.com>
2016-02-23 23:26:40 +0300

    test: use `rewire` for DI

--- test/test.js
+++ test/test.js
@@ -1,30 +1,37 @@
 'use strict';
 
-var npmPrefix = require('..');
-
 var test = require('tape'),
-    homedir = require('os-homedir');
+    homedir = require('os-homedir'),
+    rewire = require('rewire');
 
 var path = require('path');
 
 
+var prefix = rewire('..');
+
+var npmrc = {};
+
+prefix.__set__('rc', function (name) {
+  if (name == 'npm') return npmrc;
+});
+
+
 test('absolute prefix', function (t) {
-  process.chdir(__dirname + '/fixtures/absolute');
-  t.equal(npmPrefix(), '/root');
+  npmrc.prefix = '/root';
+  t.equal(prefix(), '/root');
   t.end();
 });
 
 
 test('home-relative prefix', function (t) {
-  process.chdir(__dirname + '/fixtures/home-relative');
-  t.equal(npmPrefix(), path.join(homedir(), 'local'));
+  npmrc.prefix = '~/local';
+  t.equal(prefix(), path.join(homedir(), 'local'));
   t.end();
 });
 
 
 test('no rc', function (t) {
-  process.chdir(__dirname + '/fixtures/no-rc');
-  var prefix = npmPrefix();
-  t.ok(path.relative(path.resolve(process.execPath, '../..'), prefix)[0] != '.');
+  delete npmrc.prefix;
+  t.ok(path.relative(path.resolve(process.execPath, '../..'), prefix())[0] != '.');
   t.end();
 });
